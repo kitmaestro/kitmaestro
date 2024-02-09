053a26848b8ff415b94bb5ee648d3a9569ad2dba
Otoniel Reyes <kenliten@otonielreyes.com>
2024-02-09 18:11:13 -0400

    Updated home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { AppEntry } from '../interfaces/app-entry';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatCardModule,
     MatGridListModule,
+    MatBadgeModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  apps: AppEntry[] = [
+    { name: 'Generador de Calificaciones', description: 'Genera facilmente las calificaciones de tus estudiantes.', link: ['/apps', 'grade-generator'], premium: false, icon: '/assets/grades.svg' },
+  ];
 }
---
+++ src/app/interfaces/app-entry.ts
@@ -0,0 +1,7 @@
+export interface AppEntry {
+    name: string;
+    description: string;
+    link: string[];
+    icon: string;
+    premium: boolean;
+}
