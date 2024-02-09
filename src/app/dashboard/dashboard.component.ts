d81381a98d6aba9ea1e0ce00ee5a006550a932ca
Otoniel Reyes <kenliten@otonielreyes.com>
2024-02-09 17:09:13 -0400

    Logout, Goodbye on logout, saving in saving state (user settings), outline style (login)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { UserSettingsComponent } from '../user-settings/user-settings.component';
@@ -7,6 +7,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { Auth, signOut } from '@angular/fire/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -25,5 +27,10 @@ import { RouterModule } from '@angular/router';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  auth = inject(Auth);
+  sb = inject(MatSnackBar);
 
+  logout() {
+    signOut(this.auth).then(() => this.sb.open('Haz cerrado sesión. Hasta luego!', 'Ok', { duration: 4000 }));
+  }
 }
