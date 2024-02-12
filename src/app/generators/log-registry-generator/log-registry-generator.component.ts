98b2eefe8f6f9b8009d1f9139ccba1f41d7c4864
Otoniel Reyes <kenliten@otonielreyes.com>
2024-02-12 07:01:38 -0400

    Added chart component

--- src/app/generators/log-registry-generator/log-registry-generator.component.ts
+++ src/app/generators/log-registry-generator/log-registry-generator.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { InProgressComponent } from '../../in-progress/in-progress.component';
+import { Chart, registerables } from 'chart.js';
+import { ChartComponent } from '../../ui/chart/chart.component';
 
 @Component({
   selector: 'app-log-registry-generator',
   standalone: true,
   imports: [
     InProgressComponent,
+    ChartComponent,
   ],
   templateUrl: './log-registry-generator.component.html',
   styleUrl: './log-registry-generator.component.scss'
 })
 export class LogRegistryGeneratorComponent {
-
+  data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  };
 }
