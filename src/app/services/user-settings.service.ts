42e4d3824fe81b0060266d381a5ea20623dd2f43
Otoniel Reyes <orgalay.dev@gmail.com>
2024-03-10 07:17:55 -0400

    Fixed user settings service

--- src/app/services/user-settings.service.ts
+++ src/app/services/user-settings.service.ts
@@ -1,8 +1,27 @@
 import { Injectable, inject } from '@angular/core';
 import { Auth, User, authState } from '@angular/fire/auth';
-import { Firestore, collection, collectionData, doc, query, updateDoc, where } from '@angular/fire/firestore';
-import { EMPTY, Observable, map } from 'rxjs';
+import { DocumentReference, Firestore, addDoc, collection, collectionData, doc, docData, query, updateDoc, where } from '@angular/fire/firestore';
+import { EMPTY, Observable, concatAll, from, map, of } from 'rxjs';
 import { UserSettings } from '../interfaces/user-settings';
+
+const defaultUserSettings: UserSettings = {
+  uid: '',
+  id: '',
+  title: '',
+  firstname: '',
+  lastname: '',
+  gender: '',
+  phone: '',
+  schoolName: '',
+  district: '',
+  regional: '',
+  grades: '',
+  subjects: '',
+  photoURL: '',
+  likedResources: [],
+  dislikedResources: [],
+  bookmarks: [],
+};
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +32,21 @@ export class UserSettingsService {
   private firestore = inject(Firestore);
   private settingsColRef = collection(this.firestore, 'user-settings');
   private user$ = authState(this.auth);
-  private user: User | null = null;
 
-  constructor() {
-    const sus = this.user$.subscribe(user => {this.user = user; sus.unsubscribe();});
+  createSettings(data: UserSettings = defaultUserSettings): Observable<UserSettings> {
+    return this.user$.pipe(
+      map(user => {
+        if (user) {
+          data.uid = user.uid;
+          return from(addDoc(this.settingsColRef, data) as Promise<DocumentReference<UserSettings>>).pipe(
+            map(ref => docData(doc(this.firestore, 'user-settings/' + ref.id), { idField: 'id' }) as Observable<UserSettings>),
+            concatAll()
+          );
+        }
+        return EMPTY;
+      }),
+      concatAll()
+    )
   }
 
   getSettings(userId?: string): Observable<UserSettings> {
@@ -24,19 +54,32 @@ export class UserSettingsService {
       return (collectionData(query(this.settingsColRef, where('uid', '==', userId)), { idField: 'id' }) as Observable<UserSettings[]>).pipe(
         map(subs => {
           const settings = subs[0];
-          return settings;
-        })
+          if (settings) {
+            return of(settings);
+          }
+          return this.createSettings()
+        }),
+        concatAll()
       )
     }
-    if (this.user) {
-      return (collectionData(query(this.settingsColRef, where('uid', '==', this.user.uid)), { idField: 'id' }) as Observable<UserSettings[]>).pipe(
-        map(subs => {
-          const settings = subs[0];
-          return settings;
-        })
-      )
-    }
-    return EMPTY;
+    return this.user$.pipe(
+      map(user => {
+        if (user) {
+          return (collectionData(query(this.settingsColRef, where('uid', '==', user.uid)), { idField: 'id' }) as Observable<UserSettings[]>).pipe(
+            map(subs => {
+              const settings = subs[0];
+              if (settings) {
+                return of(settings);
+              }
+              return this.createSettings()
+            }),
+            concatAll()
+          )
+        }
+        return EMPTY;
+      }),
+      concatAll(),
+    )
   }
 
   setPhotoUrl(photoURL: string): Observable<Promise<boolean>> {
--- src/app/services/user-subscription.service.ts
+++ src/app/services/user-subscription.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, User, authState } from '@angular/fire/auth';
+import { Auth, authState } from '@angular/fire/auth';
 import { Firestore, collection, collectionData, query, where } from '@angular/fire/firestore';
-import { Observable, map, of } from 'rxjs';
+import { Observable, concatAll, map, of } from 'rxjs';
 import { UserSubscription } from '../interfaces/user-subscription';
 
 @Injectable({
@@ -13,24 +13,18 @@ export class UserSubscriptionService {
   private firestore = inject(Firestore);
   private subscriptionColRef = collection(this.firestore, 'user-subscriptions');
   private user$ = authState(this.auth);
-  private user: User | null = null;
-
-  constructor() {
-    this.user$.subscribe(user => this.user = user);
-  }
 
   isPremium(): Observable<boolean> {
-    if (this.user) {
-      return (collectionData(query(this.subscriptionColRef, where('uid', '==', this.user.uid))) as Observable<UserSubscription[]>).pipe(
-        map(subs => {
-          const userSub = subs[0];
-          if (!userSub) {
-            return false;
-          }
-          return userSub.active;
-        })
-      )
-    }
-    return of(false);
+    return this.user$.pipe(
+      map(user => {
+        if (user) {
+          return (collectionData(query(this.subscriptionColRef, where('uid', '==', user.uid))) as Observable<UserSubscription[]>).pipe(
+            map(subs => subs[0] && subs[0].active)
+          )
+        }
+        return of(false);
+      }),
+      concatAll()
+    );
   }
 }
