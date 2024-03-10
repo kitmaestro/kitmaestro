c479cb9c3ea608cf1dbbbccd5ee164f3336e4f2e
Otoniel Reyes <orgalay.dev@gmail.com>
2024-03-10 08:16:11 -0400

    Started class plan work

--- src/app/class-planning/class-plan/class-plan.component.ts
+++ src/app/class-planning/class-plan/class-plan.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IsPremiumComponent } from '../../alerts/is-premium/is-premium.component';
+import { InProgressComponent } from '../../alerts/in-progress/in-progress.component';
+import { UserSettingsService } from '../../services/user-settings.service';
+import { UserSubscriptionService } from '../../services/user-subscription.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-class-plan',
   standalone: true,
   imports: [
     IsPremiumComponent,
+    InProgressComponent,
+    AsyncPipe,
   ],
   templateUrl: './class-plan.component.html',
   styleUrl: './class-plan.component.scss'
 })
 export class ClassPlanComponent {
+  userSubscriptionService = inject(UserSubscriptionService);
 
+  working = true;
+  isPremium$ = this.userSubscriptionService.isPremium();
 }
