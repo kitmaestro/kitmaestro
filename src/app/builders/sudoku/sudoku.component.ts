2be27749520c981bc1d17571ca0d8373d8f1ef14
Otoniel Reyes <orgalay.dev@gmail.com>
2024-05-03 07:10:16 -0400

    Updated sudoku

--- src/app/builders/sudoku/sudoku.component.ts
+++ src/app/builders/sudoku/sudoku.component.ts
@@ -7,6 +7,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Sudoku } from 'sudoku-gen/dist/types/sudoku.type';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-sudoku',
@@ -15,9 +17,11 @@ import { MatCardModule } from '@angular/material/card';
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
+    MatSelectModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
+    MatChipsModule,
   ],
   templateUrl: './sudoku.component.html',
   styleUrl: './sudoku.component.scss'
@@ -28,31 +32,39 @@ export class SudokuComponent {
   fb = inject(FormBuilder);
 
   sudokuLevel = this.fb.control('easy');
+  sudokuTitle = this.fb.control('Sudoku');
+  sudokuFields = this.fb.group({
+    name: [true],
+    grade: [true],
+    date: [true],
+  });
+
   sudoku: Sudoku | null = null;
+  board: Array<number | null>[] = [];
+  solvedBoard: Array<number | null>[] = [];
 
   levels = [
-    'easy',
-    'medium',
-    'hard',
-    'expert'
+    {
+      level: 'easy',
+      label: 'Facil',
+    },
+    {
+      level: 'medium',
+      label: 'Medio',
+    },
+    {
+      level: 'hard',
+      label: 'Dificil',
+    },
+    {
+      level: 'expert',
+      label: 'Experto',
+    }
   ];
 
   generate() {
     this.sudoku = this.sudokuService.generate(this.sudokuLevel.value as 'easy' | 'medium' | 'hard' | 'expert');
-    this.stringToGrid(this.sudoku.puzzle);
-  }
-
-  stringToGrid(val: string) {
-    const arr = val.split('');
-    const board: Array<string[]> = [];
-
-    for (let i = 0; i < 9; i++) {
-      board.push([]);
-      for (let j = 0; j < 9; j++) {
-        board[i].push(arr[i + j]);
-      }
-    }
-
-    console.log(board)
+    this.board = this.sudokuService.string_to_board(this.sudoku.puzzle);
+    this.solvedBoard = this.sudokuService.string_to_board(this.sudoku.solution);
   }
 }
--- src/app/services/sudoku.service.ts
+++ src/app/services/sudoku.service.ts
@@ -11,4 +11,67 @@ export class SudokuService {
   generate(level: 'easy' | 'medium' | 'hard' | 'expert') {
     return getSudoku(level);
   }
+
+  string_to_board(board_string: string): Array<number | null>[] {
+    const rows: Array<number | null>[] = [];
+    let cur_row: Array<number | null> = [];
+    const board = board_string.split('');
+
+    board.forEach((el, i) => {
+      cur_row.push(el == '-' ? null : +el);
+      if (i % 9 == 8) {
+        rows.push(cur_row);
+        cur_row = [];
+      }
+    });
+    const boxes: Array<number | null>[] = [
+      [
+        rows[0].slice(0, 3),
+        rows[1].slice(0, 3),
+        rows[2].slice(0, 3),
+      ].flat(),
+      [
+        rows[0].slice(3, 6),
+        rows[1].slice(3, 6),
+        rows[2].slice(3, 6),
+      ].flat(),
+      [
+        rows[0].slice(6),
+        rows[1].slice(6),
+        rows[2].slice(6),
+      ].flat(),
+      [
+        rows[3].slice(0, 3),
+        rows[4].slice(0, 3),
+        rows[5].slice(0, 3),
+      ].flat(),
+      [
+        rows[3].slice(3, 6),
+        rows[4].slice(3, 6),
+        rows[5].slice(3, 6),
+      ].flat(),
+      [
+        rows[3].slice(6),
+        rows[4].slice(6),
+        rows[5].slice(6),
+      ].flat(),
+      [
+        rows[6].slice(0, 3),
+        rows[7].slice(0, 3),
+        rows[8].slice(0, 3),
+      ].flat(),
+      [
+        rows[6].slice(3, 6),
+        rows[7].slice(3, 6),
+        rows[8].slice(3, 6),
+      ].flat(),
+      [
+        rows[6].slice(6),
+        rows[7].slice(6),
+        rows[8].slice(6),
+      ].flat(),
+    ];
+
+    return boxes;
+  }
 }
