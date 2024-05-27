aef8494ecbd19a2f6e1b1e258b25627ddb970352
Otoniel Reyes <orgalay.dev@gmail.com>
2024-05-27 01:56:17 -0400

    Added universal activities

---
+++ src/app/interfaces/universal-activity.ts
@@ -0,0 +1,9 @@
+import { UniversalActivityRequirement } from "./universal-activity-requirement";
+
+
+export interface UniversalActivity {
+    name: string;
+    description: string;
+    ready: boolean;
+    requirements: UniversalActivityRequirement[];
+}
