f25522b0cddb9393d1e130d4d591bfb117925656
Otoniel Reyes <orgalay.dev@gmail.com>
2024-09-29 23:02:19 -0400

    Updated settings service

--- src/app/services/user-settings.service.ts
+++ src/app/services/user-settings.service.ts
@@ -2,33 +2,34 @@ import { Injectable, inject, isDevMode } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserSettings } from '../interfaces/user-settings';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ApiUpdateResponse } from '../interfaces/api-update-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserSettingsService {
   private http = inject(HttpClient);
-  private headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-  });
-
-  private user$: Observable<UserSettings> = this.getSettings();
+  private config = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+    })
+  };
 
   private apiBaseUrl = isDevMode() ? 'http://localhost:3000/' : 'http://api.kitmaestro.com/';
 
   getSettings(userId?: string): Observable<UserSettings> {
     if (userId) {
-      return this.http.get<UserSettings>(this.apiBaseUrl + 'users/' + userId, { headers: this.headers });
+      return this.http.get<UserSettings>(this.apiBaseUrl + 'users/' + userId, this.config);
     }
-    return this.http.get<UserSettings>(this.apiBaseUrl + 'auth/profile', { headers: this.headers });
+    return this.http.get<UserSettings>(this.apiBaseUrl + 'auth/profile', this.config);
   }
 
-  update(user: UserSettings) {
-    return this.http.patch(this.apiBaseUrl + 'auth/profile', user, { headers: this.headers });
+  update(user: any): Observable<ApiUpdateResponse> {
+    return this.http.patch<ApiUpdateResponse>(this.apiBaseUrl + 'auth/profile', user, this.config);
   }
 
-  setPhotoUrl(photoURL: string): Observable<any> {
-    return this.http.patch<any>(this.apiBaseUrl + 'auth/profile', { photoURL }, { headers: this.headers });
+  setPhotoUrl(photoURL: string): Observable<ApiUpdateResponse> {
+    return this.http.patch<ApiUpdateResponse>(this.apiBaseUrl + 'auth/profile', { photoURL }, this.config);
   }
 }
