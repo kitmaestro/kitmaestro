4a3399a3118b698b313d653a514f1457651213bd
Otoniel Reyes <orgalay.dev@gmail.com>
2024-10-09 09:34:15 -0400

    Estimation scale update

--- src/app/assessments/estimation-scale/estimation-scale.component.ts
+++ src/app/assessments/estimation-scale/estimation-scale.component.ts
@@ -4,8 +4,15 @@ import { AuthService } from '../../services/auth.service';
 import { UserSettings } from '../../interfaces/user-settings';
 import { EstimationScaleService } from '../../services/estimation-scale.service';
 import { ClassSectionService } from '../../services/class-section.service';
+import { CompetenceService } from '../../services/competence.service';
 import { Router } from '@angular/router';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ClassSection } from '../../interfaces/class-section';
 
 @Component({
@@ -14,12 +21,19 @@ import { ClassSection } from '../../interfaces/class-section';
   imports: [
     ReactiveFormsModule,
     MatSnackBarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './estimation-scale.component.html',
   styleUrl: './estimation-scale.component.scss'
 })
 export class EstimationScaleComponent implements OnInit {
   private estimationScaleService = inject(EstimationScaleService);
+  private competenceService = inject(CompetenceService);
   private sectionService = inject(ClassSectionService);
   private authService = inject(AuthService);
   private router = inject(Router);
@@ -28,6 +42,7 @@ export class EstimationScaleComponent implements OnInit {
 
   public user: UserSettings | null = null;
   public sections: ClassSection[] = [];
+  public subjects: string[] = [];
   
   public scaleForm = this.fb.group({
     title: [''],
@@ -37,7 +52,11 @@ export class EstimationScaleComponent implements OnInit {
     achievementIndicators: [''],
     activity: [''],
     criteria: [''],
-    levels: [''],
+    levels: this.fb.array([
+      this.fb.control('Iniciado'),
+      this.fb.control('En Proceso'),
+      this.fb.control('Logrado'),
+    ]),
   });
 
   ngOnInit(): void {
@@ -67,5 +86,44 @@ export class EstimationScaleComponent implements OnInit {
         });
       }
     });
+  }
+
+  pretifySubject(name: string) {
+    switch (name) {
+      case 'LENGUA_ESPANOLA':
+        return 'Lengua Española';
+      case 'MATEMATICA':
+        return 'Matemática';
+      case 'CIENCIAS_SOCIALES':
+        return 'Ciencias Sociales';
+      case 'CIENCIAS_NATURALES':
+        return 'Ciencias de la Naturaleza';
+      case 'INGLES':
+        return 'Inglés';
+      case 'FRANCES':
+        return 'Francés';
+      case 'FORMACION_HUMANA':
+        return 'Formación Integral Humana y Religiosa';
+      case 'EDUCACION_FISICA':
+        return 'Educación Física';
+      case 'EDUCACION_ARTISTICA':
+        return 'Educación Artística';
+      default:
+        return 'Talleres Optativos';
+    }
+  }
+
+  onSectionSelect(event: any) {
+    const { value } = event;
+    if (!value) {
+      this.subjects = [];
+      return;
+    }
+    const section = this.sections.find(s => s._id == value);
+    if (section) {
+      this.subjects = (section.subjects as any);
+      return;
+    }
+    return [];
   }
 }
