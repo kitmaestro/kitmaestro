43d07642fd6e492fa11b625c2c14e5dd0083d2c3
Otoniel Reyes <otoniel@otonielreyes.com>
2025-09-05 06:48:01 -0400

    Added daily to unitplan

---
+++ src/app/core/interfaces/unit-plan-instruments.ts
@@ -0,0 +1,13 @@
+import { Checklist } from "./checklist";
+import { EstimationScale } from "./estimation-scale";
+import { Rubric } from "./rubric";
+import { Test } from "./test";
+import { UnitPlan } from "./unit-plan";
+
+export interface UnitPlanInstruments {
+    unitPlan: UnitPlan;
+    rubrics: Rubric[];
+    checklists: Checklist[];
+    estimationScales: EstimationScale[];
+    tests: Test[];
+}
